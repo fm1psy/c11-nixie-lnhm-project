7d956b01c6446e79facf342e85627e23c257da46
fm1psy <freddymateo1@gmail.com>
2024-06-12 13:25:29 +0100

    add more extract tests added tests that raised coverage to 94% across the extract and test_extract files This code should be able to address most edge cases, and produce a file that can be used in the transform step.

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,37 +1,38 @@
 """ This extract file connects to an API: https://data-eng-plants-api.herokuapp.com/plants/8
 and collates all of the data on the plants available. It then stores these in a single json file."""
 import time
-import requests
 import json
+import requests
 BASE_URL = "https://data-eng-plants-api.herokuapp.com/plants/"
 EXTRACT_DESTINATION = "extracted_plants.json"
-
 DEFAULT_RANGE = 50
 
 
-def get_plant_response(id: int) -> requests.Response:
+def get_plant_response(plant_id: int) -> requests.Response:
     """Return a response object."""
-    if not isinstance(id, int):
+    if not isinstance(plant_id, int):
         raise ValueError
-    return requests.get(BASE_URL+f"{id}")
+    return requests.get(BASE_URL+f"{plant_id}", timeout=10)
 
 
 def get_range_of_plants(plant_range: int = DEFAULT_RANGE) -> list[dict]:
-    """Go through a range of plant ids and return a list """
+    """Go through a range of plant ids and return a list."""
     res = []
     for i in range(1, plant_range+1):
-        res.append(get_plant_response(i).json())
+        try:
+            res.append(get_plant_response(i).json())
+        except ValueError:
+            pass
     return res
 
 
 def extract_data() -> None:
-    """Connect to the plants api and extract the plant data."""
-    plant_jsons = get_range_of_plants()
+    """Connect to the plants api and extract the plant data.
+    Then dump it onto a json file to be used in the transform step."""
+    plant_data = get_range_of_plants()
     with open(EXTRACT_DESTINATION, "w", encoding="utf-8") as f:
-        json.dump(plant_jsons, f, indent=4)
+        json.dump(plant_data, f, indent=4)
 
 
 if __name__ == "__main__":
-    start = time.time()
     extract_data()
-    print(time.time() - start)
--- pipeline/test_extract.py
+++ pipeline/test_extract.py
@@ -1,58 +1,69 @@
 # pylint: ignore-pylint
 import pytest
-import json
-import requests
-from extract import get_plant_response, get_range_of_plants
-from unittest.mock import MagicMock, patch
+from extract import get_plant_response, get_range_of_plants, extract_data
+from unittest.mock import Mock, patch, mock_open
+EXTRACT_DESTINATION = "extracted_plants.json"
 
 
+@pytest.fixture
 def mock_response(
         status=200,
         content="CONTENT",
         json_data=None):
-    mock_resp = MagicMock()
+    mock_resp = Mock()
     # set status code and content
     mock_resp.status_code = status
     mock_resp.content = content
     # add json data if provided
     if json_data:
-        mock_resp.json = MagicMock(
-            return_value=json_data
-        )
+        mock_resp.json = Mock(return_value=json_data)
+
     return mock_resp
 
 
 @patch("extract.requests.get")
-def test_get_response_is_called(mock_request):
-    mock_request.return_value = mock_response()
+def test_get_response_is_called(mock_request, mock_response):
+    mock_request.return_value = mock_response
     get_plant_response(3)
 
     assert mock_request.called == True
 
 
 @patch("extract.requests.get")
-def test_get_response_success(mock_request):
-    mock_request.return_value = mock_response()
+def test_get_response_success(mock_request, mock_response):
+    mock_request.return_value = mock_response
     response = get_plant_response(3)
     assert response.status_code == 200
 
 
 @patch("extract.requests.get")
-def test_get_response_failure(mock_request):
-    mock_request.return_value = mock_response(status=404)
-    response = get_plant_response(3)
-    assert response.status_code == 404
-
-
-@patch("extract.requests.get")
-def test_get_response_error(mock_request):
-    mock_request.return_value = mock_response()
+def test_get_response_error(mock_request, mock_response):
+    mock_request.return_value = mock_response
     with pytest.raises(ValueError):
         get_plant_response("three")
 
 
 @patch("extract.requests.get")
-def test_get_range_of_plants(mock_request):
-    mock_request.return_value = mock_response()
+def test_get_range_of_plants(mock_request, mock_response):
+    mock_request.return_value = mock_response
     get_range_of_plants(2)
     assert mock_request.call_count == 2
+
+
+@patch("extract.requests.get")
+def test_get_range_of_plants_success(mock_request, mock_response):
+    mock_request.return_value = mock_response
+    res = get_range_of_plants(2)
+    assert len(res) == 2
+
+
+@patch("builtins.open", new_callable=mock_open, read_data="data")
+@patch("extract.json.dump")
+@patch("extract.get_range_of_plants")
+def test_extract_data(mock_plants, mock_dump, mock_file):
+    mock_plants.return_value = [{}, {}]
+    extract_data()
+
+    mock_plants.assert_called_once()
+    mock_file.assert_called_once()
+    mock_dump.assert_called_once()
