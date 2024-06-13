b598e43291c974eba9df1c89416f71efb4b21742
punima-s <162708600+punima-s@users.noreply.github.com>
2024-06-13 18:32:37 +0100

    Create Pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -3,6 +3,15 @@ from extract import extract_data
 from transform import transform
 from load import loading_main
 
+
+def lambda_handler(event=None, context=None) -> dict:
+    """ LAMBDA HANDLER!!! """
+    extract_data()
+    transform()
+    loading_main()
+    return {"status": "SUCCESSS!!!!!"}
+
+
 if __name__ == "__main__":
     extract_data()
     transform()
