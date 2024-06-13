23e2814d98779789247d3135ee95551cc001f1a9
fm1psy <freddymateo1@gmail.com>
2024-06-13 17:40:07 +0100

    add schedulers to terraform added schedulers to terraform to ensure that the lambda functions are called at specific times added lambda functions for the etl pipeline and the translocation of data from sts to lts.

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ resource "aws_ecs_cluster" "c11-cluster" {
     name = "c11-cluster"
 }
 
-# ETL lambda
+# roles and permissions
 
 data "aws_iam_policy_document" "assume_role" {
   statement {
@@ -28,7 +28,7 @@ resource "aws_iam_role" "iam_for_lambda" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-
+# ETL lambda
 resource "aws_lambda_function" "c11-nixie-lnhm-etl" {
     function_name = "c11-nixie-lnhm-etl"
     role          = aws_iam_role.iam_for_lambda.arn
@@ -49,7 +49,61 @@ resource "aws_lambda_function" "c11-nixie-lnhm-etl" {
     }
 }
 
+# translocation lambda
 resource "aws_s3_bucket" "c11-nixie-lts-bucket" {
     bucket = "nixie-lmnh-plants-lts"
-    force_destroy = True
+    force_destroy = true
 }
+
+resource "aws_lambda_function" "c11-nixie-lnhm-data-translocation" {
+    function_name = "c11-nixie-lnhm-data-translocation"
+    role          = aws_iam_role.iam_for_lambda.arn
+    package_type  = "Image"
+    image_uri     = var.TRANSLOCATION_URI
+    architectures = ["x86_64"]
+    timeout       = 15
+
+    environment {
+        variables = {
+        DB_HOST     = var.DB_HOST
+        DB_NAME     = var.DB_NAME
+        DB_PASSWORD = var.DB_PASSWORD
+        DB_PORT     = var.DB_PORT
+        DB_SCHEMA   = var.DB_SCHEMA
+        DB_USER     = var.DB_USER
+        }
+    }
+}
+# schedulers
+resource "aws_scheduler_schedule" "c11-nixie-lnhm-etl-scheduler" {
+  name       = "c11-nixie-lnhm-etl-scheduler"
+  group_name = "default"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression = "cron(* * * * ? *)"
+
+  target {
+    arn      = aws_lambda_function.c11-nixie-lnhm-etl.arn
+    role_arn = aws_iam_role.iam_for_lambda.arn
+  }
+}
+
+resource "aws_scheduler_schedule" "c11-nixie-lnhm-translocation-schedule" {
+  name       = "c11-nixie-lnhm-translocation-schedule"
+  group_name = "default"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression = "cron(0 9 * * ? *)"
+
+  target {
+    arn      = aws_lambda_function.c11-nixie-lnhm-data-translocation.arn
+    role_arn = aws_iam_role.iam_for_lambda.arn
+  }
+}
+# dashboard service
